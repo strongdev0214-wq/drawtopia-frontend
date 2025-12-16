41fb8fe00ebb5ad7edc10d66b29acc75e648eabf
strongdev214 <strongdev0214@gmail.com>
2025-12-16 06:18:32 -0500

    fix: vite config ssr

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Ensure ESM-only Supabase client is bundled for SSR builds (Vercel)
+	ssr: {
+		noExternal: ['@supabase/supabase-js']
+	}
 });
